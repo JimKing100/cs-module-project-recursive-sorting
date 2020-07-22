875593cb0df7631e116809328d8cdb6e1bc521ac
JimKing100 <JimKing100@users.noreply.github.com>
2020-07-22 09:55:35 -0700

    recursive sorting

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -51,9 +51,45 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
-# def merge_in_place(arr, start, mid, end):
-    # Your code here
+
+# Merge two subarrays in merge_in_place
+# 1st subarray is arr[start..mid]
+# 2nd subarray is arr[mid+1..end]
 
 
-# def merge_sort_in_place(arr, l, r):
-    # Your code here
+def merge_in_place(arr, start, mid, end):
+    start1 = mid + 1
+
+    # If the direct merge is aready sorted
+    if arr[mid] <= arr[start1]:
+        return
+
+    # Two pointers (start and start1) to start of subarrays
+    while start <= mid and start1 <= end:
+        if arr[start] <= arr[start1]:
+            start += 1
+        else:
+            value = arr[start1]
+            index = start1
+
+            # Shift elements between element 1 and element 2 right 1
+            while index != start:
+                arr[index] = arr[index - 1]
+                index -= 1
+
+            arr[start] = value
+
+            # Increment pointers
+            start += 1
+            mid += 1
+            start1 += 1
+
+
+def merge_sort_in_place(arr, l, r):
+    if l < r:
+        m = (l + r) // 2
+
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m + 1, r)
+
+        merge_in_place(arr, l, m, r)
--- src/sorting/tester.py
+++ src/sorting/tester.py
@@ -1,4 +1,4 @@
-from sorting import merge, merge_sort
+from sorting import merge, merge_sort, merge_sort_in_place
 
 arr1 = [1, 2, 3]
 arr2 = [4, 5, 6]
@@ -9,3 +9,6 @@ print(result)
 arr3 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 result = merge_sort(arr3)
 print(result)
+
+merge_sort_in_place(arr3, 0, len(arr3) - 1)
+print(arr3)
